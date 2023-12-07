763b05148706ab212f26e0ee9858c73a157d4e91
mirvarimuradova <mirvarimuradova7@gmail.com>
2023-12-07 22:20:56 +0400

    film

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Movies from './Components/Movies'
+import { Route, Routes } from 'react-router-dom';
+import Details from './Components/Details';
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+      <Routes>
+        <Route path='/' element={<Movies/>}/>
+
+        <Route path='/details/:id' element={<Details/>}/>
+          
+        <Route/>
+        <Route/>
+          
+        <Route/>
+      </Routes>
+     
     </div>
   );
 }
